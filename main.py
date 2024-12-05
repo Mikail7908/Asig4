c169fb6850a4b4049bbf21769cb9b0e141a74726
mikail bhana <mikailbhana@mikails-MacBook-Pro.local>
2024-12-05 10:00:54 +0000

    Adding_main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello world')
+import logging
+from app import app
+
+def main():
+    app.run(debug=True)
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logging.info('Script started')
+    main()
+    logging.info('Script finished')
